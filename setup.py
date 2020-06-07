50196640c2edef2b7b6e03f4f900acc793f856b8
ku222 <44293915+ku222@users.noreply.github.com>
2020-06-07 21:32:26 +0100

    Published to Pip

--- setup.py
+++ setup.py
@@ -1,10 +1,28 @@
 from setuptools import setup
 
-setup = (
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
     name='adaptivecardbuilder',
     version='0.0.1',
     description='Easily Build and Export Adaptive Cards Through Python',
     py_modules=['classes'],
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
+    data_files = [("", ["LICENSE.txt"])],
+    url="https://github.com/ku222/AdaptiveCardBuilder",
+    author="Kovid Uppal",
+    author_email="kovid.uppal@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 1 - Planning"
+    ]
 )
-
